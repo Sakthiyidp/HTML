8cfc783b53c70e1ae0c3736c4475fe2fbb9184c0
Sathya Prakash <123102578+Sakthiyidp@users.noreply.github.com>
2023-02-04 22:04:23 +0530

    Add files via upload

---
+++ kgm/subtraction1.java
@@ -0,0 +1,16 @@
+package kgm;
+import java.util.*;
+public class subtraction1
+{
+public void operation()
+{
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+ System.out.println("Enter the value1");
+    a=sc.nextInt();
+    System.out.println("Enter the value2");
+    b=sc.nextInt();
+    c=a+b;
+    System.out.println("Sum of numbers"+c);
+}
+}
---
+++ kgm/subtraction2.java
@@ -0,0 +1,9 @@
+import kgm.subtraction1;
+public class subtraction2
+{
+public static void main(String args[])
+{
+subtraction1 obj=new subtraction1();
+obj.operation();
+}
+}
